687faf4c9cce136a214b801589892ec9cbc6d95d
nickciske <nickciske@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-13 22:37:19 +0000

    Updates #13609
    Wrapped raw strings for translation.
    
    git-svn-id: http://plugins.svn.wordpress.org/salesforce-wordpress-to-lead/branches/2.0@450813 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- salesforce.php
+++ salesforce.php
@@ -35,8 +35,7 @@ if ( ! class_exists( 'Salesforce_Admin' ) ) {
 		function warning() {
 			$options  = get_option($this->optionname);
 			if (!isset($options['org_id']) || empty($options['org_id']))
-				echo "<div id='message' class='error'><p><strong>Your WordPress-to-Lead  settings are not complete.</strong> You must <a href='".$this->plugin_options_url()."'>enter your Salesforce.com Organisation ID</a> for it to work.</p></div>";
-			
+				echo "<div id='message' class='error'><p><strong>".__('Your WordPress-to-Lead settings are not complete.','salesforce')."</strong> ".__('You must enter your Salesforce.com Organisation ID for it to work.','salesforce')." <a href='".$this->plugin_options_url()."'>".__('Settings','salesforce')."</a></p></div>";
 		}
 		
 		function config_page() {
@@ -127,13 +126,13 @@ if ( ! class_exists( 'Salesforce_Admin' ) ) {
 																		
 									$content = '<style type="text/css">th{text-align:left;}</style><table>';
 									$content .= '<tr>'
-									.'<th width="15%">ID</th>'
-									.'<th width="10%">Enable</th>'
-									.'<th width="10%">Required</th>'
-									.'<th width="10%">Type</th>'
-									.'<th width="20%">Label</th>'
-									.'<th width="20%">Value</th>'
-									.'<th width="10%">Position</th>'
+									.'<th width="15%">'.__('ID','salesforce').'</th>'
+									.'<th width="10%">'.__('Enable','salesforce').'</th>'
+									.'<th width="10%">'.__('Required','salesforce').'</th>'
+									.'<th width="10%">'.__('Type','salesforce').'</th>'
+									.'<th width="20%">'.__('Label','salesforce').'</th>'
+									.'<th width="20%">'.__('Value','salesforce').'</th>'
+									.'<th width="10%">'.__('Position','salesforce').'</th>'
 									.'</tr>';
 									$i = 1;
 									foreach ($options['inputs'] as $id => $input) {
@@ -160,12 +159,12 @@ if ( ! class_exists( 'Salesforce_Admin' ) ) {
 								<div class="submit"><input type="submit" class="button-primary" name="submit" value="<?php _e("Save WordPress-to-Lead Settings", 'salesforce'); ?>" /></div>
 							</form>
 							<?php } else if ($_GET['tab'] == 'css') { ?>
-							<p><a href="<?php echo $this->plugin_options_url(); ?>">&laquo; Back to config page.</a></p>
-							<p>If you don't want the inline styling this plugins uses, but add the CSS for the form to your own theme's CSS, you can start by just copying the proper CSS below into your CSS file. Just copy the correct text, and then you can usually find &amp; edit your CSS file <a href="<?php echo admin_url('theme-editor.php'); ?>?file=<?php echo str_replace(WP_CONTENT_DIR,'',get_stylesheet_directory()); ?>/style.css&amp;theme=<?php echo urlencode(get_current_theme()); ?>&amp;dir=style">here</a>.</p>
+							<p><a href="<?php echo $this->plugin_options_url(); ?>">&laquo; <?php echo __('Back to config page.','salesforce'); ?></a></p>
+							<p><?php echo __("If you don't want the inline styling this plugins uses, but add the CSS for the form to your own theme's CSS, you can start by just copying the proper CSS below into your CSS file. Just copy the correct text, and then you can usually find &amp; edit your CSS file",'salesforce'); ?> <a href="<?php echo admin_url('theme-editor.php'); ?>?file=<?php echo str_replace(WP_CONTENT_DIR,'',get_stylesheet_directory()); ?>/style.css&amp;theme=<?php echo urlencode(get_current_theme()); ?>&amp;dir=style"><?php echo __('here','salesforce');?></a>.</p>
 							<div style="width:260px;margin:0 10px 0 0;float:left;">
 								<div id="normalcss" class="postbox">
-									<div class="handlediv" title="Click to toggle"><br /></div>
-									<h3 class="hndle"><span>CSS for the normal form</span></h3>
+									<div class="handlediv" title="<?php echo __('Click to toggle','salesforce'); ?>"><br /></div>
+									<h3 class="hndle"><span><?php echo __('CSS for the normal form','salesforce'); ?></span></h3>
 									<div class="inside">
 <pre>form.w2llead {
   text-align: left;
@@ -220,8 +219,8 @@ if ( ! class_exists( 'Salesforce_Admin' ) ) {
 </div></div>
 <div style="width:260px;float:left;">
 	<div id="widgetcss" class="postbox">
-		<div class="handlediv" title="Click to toggle"><br /></div>
-		<h3 class="hndle"><span>CSS for the sidebar widget form</span></h3>
+		<div class="handlediv" title="<?php echo __('Click to toggle','salesforce'); ?>"><br /></div>
+		<h3 class="hndle"><span><?php echo __('CSS for the sidebar widget form','salesforce'); ?></span></h3>
 		<div class="inside">
 <pre>.sidebar form.w2llead {
   clear: none;
@@ -286,15 +285,15 @@ if ( ! class_exists( 'Salesforce_Admin' ) ) {
 function salesforce_default_settings() {
 	$options = array();
 	$options['version'] 			= '2.0';
-	$options['successmsg'] 			= 'Success!';
-	$options['errormsg'] 			= 'There was an error, please fill all required fields.';
-	$options['requiredfieldstext'] 	= 'These fields are required.';
-	$options['sferrormsg'] 			= 'Failed to connect to Salesforce.com.';
-	$options['source'] 				= 'Lead form on '.get_bloginfo('name');
-	$options['submitbutton']	 	= 'Submit';
-	$options['subject']	 			= 'Thank you for contacting %BLOG_NAME%';
+	$options['successmsg'] 			= __('Success!','salesforce');
+	$options['errormsg'] 			= __('There was an error, please fill all required fields.','salesforce');
+	$options['requiredfieldstext'] 	= __('These fields are required.','salesforce');
+	$options['sferrormsg'] 			= __('Failed to connect to Salesforce.com.','salesforce');
+	$options['source'] 				= __('Lead form on ','salesforce').get_bloginfo('name');
+	$options['submitbutton']	 	= __('Submit','salesforce');
+	$options['subject']	 			= __('Thank you for contacting %BLOG_NAME%','salesforce');
 	$options['showccuser'] 			= true;
-	$options['ccusermsg']			= 'Send me a copy';
+	$options['ccusermsg']			= __('Send me a copy','salesforce');
 	$options['ccadmin']				= false;
 
 	$options['usecss']				= true;
@@ -418,8 +417,8 @@ function salesforce_form($options, $is_sidebar = false, $content = '') {
 	//send me a copy
 	if( $options['showccuser'] ){
 		$label = $options['ccusermsg'];
-		if( empty($label) ) $label = 'Send me a copy';
-		$content .= "\t\n\t".'<input type="checkbox" name="w2lcc" class="w2linput checkbox" value="1"/><label class="w2llabel checkbox">'.stripslashes($label)."</label><br/>\n";
+		if( empty($label) ) $label = __('Send me a copy','salesforce');
+		$content .= "\t\n\t".'<input type="checkbox" name="w2lcc" class="w2linput checkbox" value="1"/><label class="w2llabel checkbox">'.esc_html($label)."</label><br/>\n";
 	}
 	
 	//spam honeypot
@@ -444,7 +443,7 @@ function salesforce_form($options, $is_sidebar = false, $content = '') {
 	$reqtext = stripslashes($options['requiredfieldstext']);
 	if (!empty($reqtext))
 		$content .= '<p id="requiredfieldsmsg"><sup>*</sup>'.esc_html($reqtext).'</p>';
-	$content .= '<div id="salesforce"><small>Powered by <a href="http://www.salesforce.com/">Salesforce CRM</a></small></div>';
+	$content .= '<div id="salesforce"><small>'.__('Powered by','salesforce').' <a href="http://www.salesforce.com/">Salesforce CRM</a></small></div>';
 	return $content;
 }
 
@@ -499,7 +498,7 @@ function salesforce_cc_user($post, $options){
 	$headers = 'From: '.get_bloginfo('name').' <' . get_option('admin_email') . ">\r\n";
 
 	$subject = str_replace('%BLOG_NAME%', get_bloginfo('name'), $options['subject']);
-	if( empty($subject) ) $subject = 'Thank you for contacting '.get_bloginfo('name');
+	if( empty($subject) ) $subject = __('Thank you for contacting','salesforce').' '.get_bloginfo('name');
 
 	//remove hidden fields
 	foreach ($options['inputs'] as $id => $input) {
@@ -525,7 +524,7 @@ function salesforce_cc_admin($post, $options){
 	
 	$headers = 'From: '.get_bloginfo('name').' <' . get_option('admin_email') . ">\r\n";
 
-	$subject = 'Salesforce WP to Lead Submission';
+	$subject = __('Salesforce WP to Lead Submission','salesforce');
 
 	unset($post['oid']);
 	unset($post['lead_source']);
@@ -569,7 +568,7 @@ function salesforce_form_shortcode($is_sidebar = false) {
 		} else {
 			$errormsg = esc_html( stripslashes($options['errormsg']) ) ;
 			if ($emailerror)
-				$errormsg .= '<br/>The email address you entered is not a valid email address.';
+				$errormsg .= '<br/>'.__('The email address you entered is not a valid email address.','salesforce');
 			$content = salesforce_form($options, $is_sidebar, $errormsg);
 		}
 	} else {
@@ -582,7 +581,7 @@ add_shortcode('salesforce', 'salesforce_form_shortcode');
 class Salesforce_WordPress_to_Lead_Widgets extends WP_Widget {
 
 	function Salesforce_WordPress_to_Lead_Widgets() {
-		$widget_ops = array( 'classname' => 'salesforce', 'description' => 'Displays a WordPress-to-Lead for Salesforce Form' );
+		$widget_ops = array( 'classname' => 'salesforce', 'description' => __('Displays a WordPress-to-Lead for Salesforce Form','salesforce') );
 		$control_ops = array( 'width' => 200, 'height' => 250, 'id_base' => 'salesforce' );
 		$this->WP_Widget( 'salesforce', 'Salesforce', $widget_ops, $control_ops );
 	}
